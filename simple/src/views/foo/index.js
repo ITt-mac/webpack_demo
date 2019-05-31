4cdba5f72510abbd1bf26fee584f4e92dc972f8a
hcf <iloveyout-mac@qq.com>
2019-05-31 19:10:14 +0800

    添加了部分功能代码

---
+++ simple/src/views/foo/index.js
@@ -0,0 +1,13 @@
+import router from '../../router'
+
+// 引入 html 模板，会被作为字符串引入
+import tamplate from './index.html'
+
+import './style.css'
+
+//导出类
+export default class {
+    mount(container) {
+
+    }
+}
